bfb4ec86d4e8fd1081b9bce508b7f16265f3ef8b
Noman Nasir Minhas <sp17bsse010@gmail.com>
2020-08-24 19:55:53 +0500

    Minor Fixes

--- Middleware/Rust/author/src/api_author/announce.rs
+++ Middleware/Rust/author/src/api_author/announce.rs
@@ -1,12 +1,16 @@
-use iota_streams::app_channels::api::tangle::{Author, Transport, Address};
 use failure::Fallible;
+use iota_streams::app_channels::api::tangle::{Address, Author, Transport};
 
-pub fn start_a_new_channel<T: Transport>(author: &mut Author, client: &mut T, send_opt: T::SendOptions) -> Fallible<Address> {
-//this function takes a generic type 
-//that implements the Transport trait for sending and receiving messages on a communication channel.
+pub fn start_a_new_channel<T: Transport>(
+    author: &mut Author,
+    client: &mut T,
+    send_opt: T::SendOptions,
+) -> Fallible<Address> {
+    //this function takes a generic type
+    //that implements the Transport trait for sending and receiving messages on a communication channel.
 
-let announcement = author.announce()?;
-client.send_message_with_options(&announcement, send_opt)?;
-println!("Channel published");
-Ok(announcement.link)
+    let announcement = author.announce()?;
+    client.send_message_with_options(&announcement, send_opt)?;
+    println!("Channel published");
+    Ok(announcement.link)
 }
--- Middleware/Rust/author/src/main.rs
+++ Middleware/Rust/author/src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
-use iota_streams::{app_channels::api::tangle::{Author}};
 use iota_lib_rs::prelude::iota_client;
 use iota_streams::app::transport::tangle::client::SendTrytesOptions;
+use iota_streams::app_channels::api::tangle::Author;
 mod api_author;
 use crate::api_author::announce::start_a_new_channel;
 //use crate::api_author::send_message::send_signed_message;
@@ -9,18 +9,21 @@ fn main() {
     //println!("Hello, world!");
     let mut client = iota_client::Client::new("https://nodes.devnet.iota.org:443");
 
-let mut send_opt = SendTrytesOptions::default();
-send_opt.min_weight_magnitude = 9;
-send_opt.local_pow = false;
-let mut author = Author::new("OURSEEDHERE", 3, true);
-let channel_address = author.channel_address().to_string();
-// let announce_message = start_a_new_channel(&mut author, &mut client, send_opt).unwrap();
+    let mut send_opt = SendTrytesOptions::default();
+    send_opt.min_weight_magnitude = 9;
+    send_opt.local_pow = false;
+    let mut author = Author::new("OURSEEDHERE", 3, true);
+    let channel_address = author.channel_address().to_string();
+    // let announce_message = start_a_new_channel(&mut author, &mut client, send_opt).unwrap();
 
-// let public_payload = "OURMESSAGEHERE";
+    // let public_payload = "OURMESSAGEHERE";
 
-//let signed_message = send_signed_message(&mut author, &channel_address, &announce_message.msgid.to_string(), &public_payload.to_string(), &mut client, send_opt).unwrap();
+    //let signed_message = send_signed_message(&mut author, &channel_address, &announce_message.msgid.to_string(), &public_payload.to_string(), &mut client, send_opt).unwrap();
 
-println!("");
-println!("Stream Channel Published at below address\n {}", channel_address);
-println!("");
+    println!("");
+    println!(
+        "Stream Channel Published at below address\n {}",
+        channel_address
+    );
+    println!("");
 }
