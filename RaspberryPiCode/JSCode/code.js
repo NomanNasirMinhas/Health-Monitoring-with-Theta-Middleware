191419186476dda7580fff98e8065d14b316fd0a
Noman Nasir Minhas <sp17bsse010@gmail.com>
2020-11-13 19:42:05 +0500

    MAM Publish Implementation Started on Raspberry Pi

--- RaspberryPiCode/JSCode/code.js
+++ RaspberryPiCode/JSCode/code.js
@@ -1,15 +1,27 @@
+const {publishMAMmsg, sendPublicTransaction } = require('./thetamiddleware/middleware');
+require('dotenv').config();
+
+
 async function readData(){
 const fetch = require("node-fetch");
 const sensor = require('ds18b20-raspi');
 
-while(1)
-{
-var tempC = sensor.readSimpleF();
+var tempF = sensor.readSimpleF();
 var max30100 = await fetch('http://localhost:5000/');
 max30100 = await max30100.json();
-console.log(`Temp = ${tempC}F, Heart Rate = ${max30100.HR}, SPo2 = ${max30100.SPo2}`);
-
+console.log(`Temp = ${tempF}-F, Heart Rate = ${max30100.HR}, SPo2 = ${max30100.SPo2}`);
+var dataObj = {
+    Temp: tempF,
+    HR: max30100.HR,
+    SpO2: max30100.SPo2
 }
+return JSON.stringify(dataObj)
 }
 
-readData()
+async function publishMam(){
+    testSeed =process.env.testSeed;
+    testAddress =process.env.testAddress;
+    await publishMAMmsg(readData, testSeed, testAddress)
+  }
+
+publishMam()
